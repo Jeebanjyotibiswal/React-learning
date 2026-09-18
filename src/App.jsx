051c1d0ce66ce548a7801a0d223ec12c11b3023a
Jeebanjyoti Biswal <163560562+Jeebanjyotibiswal@users.noreply.github.com>
2026-09-18 15:33:45 +0530

    form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import React from "react"
-import { useState } from "react"
-import Hobbies from "./Hobbies"
-import Child from "./Child"
-import User from "./User"
-import Event from "./Event"
-const App=()=>{
-  const [hobbi,setHobbi]=useState("cricket")
-  let hobby="cricket"
-  const obj1={
-    "name":"jeeban",
-    "age":21
-  }
-  const name="jeeban"
-  let array=[1,2,3,4,5]
-  return(
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    (e.preventDefault(),
+      alert(`Name:${name},Email:${email},Password:${password}`));
+  };
+ const handleClear = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
     <div>
-      <h1>Welcome to My React APP</h1>
-      <Hobbies hobby={hobby} obj={obj1} num={array.join(", ")} />
-    
-      <Child/> {/* //default props */}
-       {/* //Jsx props */}
-     <User> 
-      <div> 
-        <h1>My name is JSX </h1>  
-      </div>
-     </User>
-     <Event />
-     
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your Name"
+        />
+
+        
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your Email"
+        />
+        
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your Password"
+        />
+        
+        <button type="submit" onSubmit={handleSubmit}>submit</button>
+        
+        <button type="reset" onClick={handleClear}>clear</button>
+        <p>LIVE demo : {name}, {email}, {password}</p>
+      </form>
     </div>
-    // Onclick Event Props
-  )
+  );
 }
-export default App
+
+export default App;
